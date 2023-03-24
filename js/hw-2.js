52393fadbba216012a6644222c8537dd45a157da
Dmitry Tel <dmitrij.teleganov@gmail.com>
2023-03-24 20:31:27 +0300

    HW-2 has done

---
+++ js/hw-2.js
@@ -0,0 +1,75 @@
+//Задание 1
+// let a = 10;
+// a = 20;
+
+// alert(a);
+
+//Задание 2
+let yearOfMadeFirstIPhone = 2007;
+
+// alert(yearOfMadeFirstIPhone);
+
+//Задание 3
+let nameOfCreaterJS = 'Brendan Eich';
+
+// alert(nameOfCreaterJS);
+
+//Задание 4
+// let a = 10;
+// let b = 2;
+
+// alert(`Сумма: ${a + b}, разность: ${a - b}, произведение: ${a * b}, частное: ${a / b}`);
+
+//Задание 5
+// let a = 2;
+// let result = a**5;
+
+// alert(result);
+
+//Задание 6
+// let a = 9;
+// let b = 2;
+
+// alert(`Остаток от деления: ${a % b}`);
+
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+
+// alert(num);
+
+//Задание 8
+// let age = +prompt('Сколько вам лет?');
+
+// alert(age);
+
+//Задание 9.0
+const user = {
+    name: 'Василий',
+    age: 25,
+    isAdmin: true
+};
+
+//Задание 9.1
+user['city of residence'] = false;
+
+//Задание 9.2
+user.age = 30;
+
+//Задание 9.3
+delete user['city of residence'];
+
+//Задание 9.4
+// let info = prompt('Какую информацию хотите узнать о пользователе?');
+
+// alert(user[info]);
+
+//Задание 10
+let yourName = prompt('Ваше имя');
+
+alert(`Привет, ${yourName}`);
