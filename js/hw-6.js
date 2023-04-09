41139a6faf72df469fd99f205649dd07a2efd6bb
Dmitry Tel <dmitrij.teleganov@gmail.com>
2023-04-09 15:42:26 +0300

    hw-6 edited finally

--- js/hw-6.js
+++ js/hw-6.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //1
-// let arr = [1, 5, 4, 10, 0, 3];
+// const arr = [1, 5, 4, 10, 0, 3];
 
 // for (let i = 0; i < arr.length; i++) {
 //     if (i <= arr.indexOf(10)) {
@@ -15,13 +15,13 @@
 // console.log(arr.indexOf(4));
 
 //3
-// let numbers = [1, 3, 5, 10, 20];
+// const numbers = [1, 3, 5, 10, 20];
 // let str = numbers.join(' ');
 
 // console.log(str);
 
 //4
-let newArr = [];
+const newArr = [];
 
 for (let i = 0; i < 3; i++) {
     let subArr = [];
@@ -36,21 +36,21 @@ for (let i = 0; i < 3; i++) {
 console.log(newArr);
 
 //5
-// let arr = [1, 1, 1];
+// const arr = [1, 1, 1];
 
 // arr.push(2, 2, 2);
 
 // console.log(arr);
 
 //6
-// let arr = [9, 8, 7, 'a', 6, 5];
+// const arr = [9, 8, 7, 'a', 6, 5];
 
 // arr.sort().pop();
 
 // console.log(arr);
 
 //7
-// let listOfNumbers = [9, 8, 7, 6, 5];
+// const listOfNumbers = [9, 8, 7, 6, 5];
 // let num = +prompt('Введите число от 1 до 10');
 
 // alert(listOfNumbers.includes(num) ? 'Есть' : 'Нет');
@@ -61,7 +61,7 @@ let string = 'abcdef';
 console.log(string.split('').reverse().join(''));
 
 //9
-// let arr = [[1, 2, 3,],[4, 5, 6]];
+// const arr = [[1, 2, 3,],[4, 5, 6]];
 // let result = [];
 
 // for (let subArr of arr) {
@@ -73,7 +73,7 @@ console.log(string.split('').reverse().join(''));
 // console.log(result);
 
 //10
-// let arr = [3, 5, 8, 6, 4, 9];
+// const arr = [3, 5, 8, 6, 4, 9];
 
 // for (let i = 0; i < arr.length - 1; i++) {
 //     console.log(arr[i] + arr[i + 1]);
@@ -84,7 +84,7 @@ console.log(string.split('').reverse().join(''));
 
 //12
 const getLengthWords = (words) => {
-    let arr = [];
+    const arr = [];
 
     for (let word of words) {
         arr.push(word.length);
@@ -99,7 +99,7 @@ console.log(getLengthWords(['слово', '', 'слог', 'длинное пре
 console.log([1, -2, 3, 5, -7, 8, -1, 0].filter(elem => elem < 0));
 
 //14
-let arr = [];
+const arr = [];
 
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + 1) + min;
@@ -109,13 +109,13 @@ for (let i = 0; i < 10; i++) {
     arr.push(getRandomNumber(0, 10));
 }
 
-let arrEven = arr.filter(elem => elem % 2 == 0);
+const arrEven = arr.filter(elem => elem % 2 == 0);
 
 console.log(arr);
 console.log(arrEven);
 
 //15
-let numbers = [];
+const numbers = [];
 
 for (let i = 0; i < 6; i++) {
     numbers.push(getRandomNumber(1, 10));
